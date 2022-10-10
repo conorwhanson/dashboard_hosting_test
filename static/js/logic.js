c76177f06f9104e32b63ce2441c65505c1d7dc46
Conor Hanson <chanson@Admins-iMac.local>
2022-10-10 09:02:14 -0500

    initial commit

---
+++ static/js/logic.js
@@ -0,0 +1,7 @@
+var password; 
+        var pass1=pass; 
+        password=prompt('Enter Password',''); 
+        if (password==pass1) 
+        alert('Correct Password! Click OK to see your data dashboard!'); 
+        else { window.location="https://google.com/"; 
+        } 
---
+++ static/js/logic2.js
@@ -0,0 +1,1 @@
+var pass = "passw0rd89!"
